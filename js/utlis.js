7a25175d4613c3158a580c1fcb44b2a4edfd5e19
Yury Lunev <yury.lunev@me.com>
2018-11-25 21:43:29 +0300

    Исправить ошибку при неправильном ответе на последний вопрос.

--- js/utlis.js
+++ js/utlis.js
@@ -1,11 +1,11 @@
 import greeting from "./screen-greeting";
 import rules from "./screen-rules";
 import game1 from "./screen-game-1";
-import {getQuestions} from "./data/game-questions";
-import {INITIAL_GAME} from "./data/game-data";
 import game2 from "./screen-game-2";
 import game3 from "./screen-game-3";
 import stats from "./screen-stats";
+import {getQuestions} from "./data/game-questions";
+import {INITIAL_GAME} from "./data/game-data";
 import {decreaseLives, hasLives, isDead} from "./data/game-lives";
 import {isFinalQuestion, nextLevel} from "./data/game-levels";
 import {addAnswer} from "./data/game-score";
@@ -15,14 +15,14 @@ const renderScreen = (screen, callback, state) => showScreen(screen(callback, st
 const getGameContent = (questions, level) => [game2, game1, game3][questions[level].length - 1];
 
 const gameTick = (state, returnValue) => {
+  if (isFinalQuestion(state)) {
+    return renderScreen(stats, addAnswer(state, returnValue));
+  }
   if (returnValue === `wrong` && isDead(decreaseLives(state))) {
     return renderScreen(stats, addAnswer(decreaseLives(state), returnValue));
   }
   if (returnValue === `wrong` && hasLives(decreaseLives(state))) {
     return onGameStart(nextLevel(decreaseLives(addAnswer(state, returnValue))));
-  }
-  if (isFinalQuestion(state)) {
-    return renderScreen(stats, addAnswer(state, returnValue));
   }
   return onGameStart(nextLevel(addAnswer(state, returnValue)));
 };
@@ -46,4 +46,4 @@ const getElementFromTemplate = (template) => {
   return result;
 };
 
-export {getElementFromTemplate, showScreen, renderScreen, onAsteriskClick, onContinueClick};
+export {getElementFromTemplate, renderScreen, onAsteriskClick, onContinueClick};
