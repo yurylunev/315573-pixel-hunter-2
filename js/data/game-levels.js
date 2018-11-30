53250fb3befec32957fcd79d6b5921519c33c1ae
Yury Lunev <yury.lunev@me.com>
2018-11-30 22:58:36 +0300

    Сделать строгое неравенство при подсчёте последнего вопроса

--- js/data/game-levels.js
+++ js/data/game-levels.js
@@ -12,8 +12,6 @@ const nextLevel = (game) => {
   return changeLevel(game, game.level + 1);
 };
 
-const isFinalQuestion = (game) => {
-  return game.level === game.questions.length - 1;
-};
+const isFinalQuestion = (game) => game.level + 1 > game.questions.length - 1;
 
 export {changeLevel, nextLevel, isFinalQuestion};
