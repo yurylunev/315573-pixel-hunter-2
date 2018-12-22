5b122c2f93e16c384054664dd59dac7e6da3cd35
Yury Lunev <yury.lunev@me.com>
2018-12-22 12:32:41 +0300

    Marty McFly's having the time of his life

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,9 @@ const rollup = require(`gulp-better-rollup`);
 const sourcemaps = require(`gulp-sourcemaps`);
 const mocha = require(`gulp-mocha`);
 const commonjs = require(`rollup-plugin-commonjs`);
+const resolve = require(`rollup-plugin-node-resolve`);
+const babel = require(`rollup-plugin-babel`);
+const uglify = require(`gulp-uglify`);
 
 gulp.task(`style`, () => {
   return gulp.src(`sass/style.scss`).
@@ -52,6 +55,21 @@ gulp.task(`scripts`, () => {
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(rollup({}, `iife`))
+    .pipe(sourcemaps.write(``))
+    .pipe(gulp.dest(`build/js`))
+    .pipe(rollup({
+      cache: false,
+      plugins: [
+        resolve({browser: true}),
+        commonjs(),
+        babel({
+          babelrc: false,
+          exclude: `node_modules/**`,
+          presets: [`@babel/env`]
+        })
+      ]
+    }, `iife`))
+    .pipe(uglify())
     .pipe(sourcemaps.write(``))
     .pipe(gulp.dest(`build/js`));
 });
